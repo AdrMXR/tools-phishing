8550bfc0fbb207baf34b618586c8388111f16fae
AdrMXR <54054887+AdrMXR@users.noreply.github.com>
2019-09-29 17:14:30 -0500

    Update tools-phishing.py

--- tools-phishing.py
+++ tools-phishing.py
@@ -17,11 +17,13 @@ def HiddenEye():
   directory = raw_input("Ingrese la ruta donde desea guardar la herramienta (Ej: /root/Desktop):\n--> ")
   os.system('cd {} && mkdir HiddenEye && git clone https://github.com/DarkSecDevelopers/HiddenEye.git'.format(directory))
   print("Instalando requerimientos...")
+  time.sleep(3)
   os.system('cd {} && chmod 777 HiddenEye && sudo apt install python3-pip && cd HiddenEye/ && sudo pip3 install -r requirements.txt'.format(directory))
   if raw_input("¿Desea ejecutar la herramienta? (y/n)\n--> ").upper() != "Y":
     print("GRACIAS POR UTILIZAR TOOLS-PHISHING").format(END) 
     exit(0)
   print("Ejecutando herramienta...")
+  time.sleep(3)
   os.system('cd {} && cd HiddenEye/ && python3 HiddenEye.py'.format(directory))
   os.system('cd {} && cd HiddenEye/ && python3 HiddenEye.py'.format(directory)) # Segunda Ejecucion de HiddenEye para evitar el problema de conexiòn de internet.
   exit(0)
@@ -30,11 +32,13 @@ def SocialPhish():
   directory = raw_input("Ingrese la ruta donde desea guardar la herramienta (Ej: /root/Desktop): \n--> ")
   os.system('cd {} && mkdir SocialPhish && git clone https://github.com/xHak9x/SocialPhish.git'.format(directory))
   print("Ingresando permisos...")
+  time.sleep(3)
   os.system('cd {} && cd SocialPhish && chmod +x socialphish.sh'.format(directory))
   if raw_input("¿Desea ejecutar la herramienta? (y/n)\n--> ").upper() != "Y":
     print("GRACIAS POR UTILIZAR TOOLS-PHISHING").format(END)
     exit(0)
   print("Ejecutando herramienta...")
+  time.sleep(3)
   os.system('clear && cd {} && cd SocialPhish && ./socialphish.sh'.format(directory))
   exit(0)
 
@@ -42,11 +46,13 @@ def SocialFish():
   directory = raw_input("Ingrese la ruta donde desea guardar la herramienta (Ej: /root/Desktop): \n--> ")
   os.system('cd {} && mkdir SocialFish && git clone https://github.com/UndeadSec/SocialFish.git'.format(directory))
   print("Instalando requerimientos...")
+  time.sleep(3)
   os.system('sudo apt-get install python3 python3-pip python3-dev -y && cd {} && cd SocialFish && python3 -m pip install -r requirements.txt '.format(directory))
   if raw_input("¿Desea ejecutar la herramienta? (y/n)\n--> ").upper() != "Y":
     print("GRACIAS POR UTILIZAR TOOLS-PHISHING").format(END)
     exit(0)
   print("Ejecutando herramienta...")
+  time.sleep(3)
   os.system('clear && cd {} && cd SocialFish && python3 SocialFish.py'.format(directory))
   exit(0)
 
@@ -57,6 +63,7 @@ def PhisherMan():
     print("GRACIAS POR UTILIZAR TOOLS-PHISHING").format(END)
     exit(0)
   print("Ejecutando herramienta...")
+  time.sleep(3)
   os.system('cd {} && cd Phisher-man && python phisherman.py'.format(directory))
   exit(0)
 
@@ -67,6 +74,7 @@ def Shellphish():
     print("GRACIAS POR UTILIZAR TOOLS-PHISHING").format(END)
     exit(0)
   print("Ejecutando herramienta...")
+  time.sleep(3)
   os.system('clear && cd {} && cd shellphish && bash shellphish.sh'.format(directory))
   exit(0)
 
@@ -74,11 +82,13 @@ def Wifiphisher():
   directory = raw_input("Ingrese la ruta donde desea guardar la herramienta (Ej: /root/Desktop): \n--> ")
   os.system('cd {} && git clone https://github.com/wifiphisher/wifiphisher.git'.format(directory))
   print("Instalando requerimientos...")
+  time.sleep(3)
   os.system('cd {} && cd wifiphisher && sudo python setup.py install'.format(directory))
   if raw_input("¿Desea ejecutar la herramienta? (y/n)\n--> ").upper() != "Y":
     print("GRACIAS POR UTILIZAR TOOLS-PHISHING").format(END)
     exit(0)
   print("Ejecutando herramienta...")
+  time.sleep(3)
   os.system('wifiphisher')
 
 def Modlishka():
@@ -86,11 +96,13 @@ def Modlishka():
   if raw_input("¿Desea continuar? (y/n)\n--> ").upper() != "Y":
     print("GRACIAS POR UTILIZAR TOOLS-PHISHING").format(END)
   print("Instalando requerimientos...")
+  time.sleep(3)
   os.system('cd /usr/local && sudo curl -O https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz && sudo tar -xzvf go1.9.linux-amd64.tar.gz && echo export PATH=$PATH:/usr/local/go/bin >> /etc/profile')
   if raw_input("Se necesita reiniciar su equipo para efectuar los cambios ¿Desea continuar? (y/n)\n--> ").upper() != "Y":
     print("GRACIAS POR UTILIZAR TOOLS-PHISHING").format(END)
     exit(0)
   print("Reiniciando equipo...")
+  time.sleep(2)
   os.system('sudo reboot')
   exit(0)
 
@@ -112,8 +124,6 @@ def menu():
   elif opcion == 7:
     Modlishka()
   else:
-    os.system('clear')
-    print("\n{}No has elegido ninguna opcion").format(RED)
     return menu()
 
 
